9add65bacfba263d2dbceeae0cb7522449d93725
Hieu5980 <tthieu8320@gmail.com>
2026-07-05 15:46:58 +0700

    add max limit

--- backend-api/src/schema/review.schema.js
+++ backend-api/src/schema/review.schema.js
@@ -3,5 +3,8 @@ const { z } = require("zod");
 exports.createReviewSchema = z.object({
   product_id: z.number().int().positive(),
   rating: z.number().int().min(1).max(5),
-  comment: z.string().optional(),
+  comment: z
+    .string()
+    .max(1500, "Nội dung đánh giá không được vượt quá 1500 ký tự")
+    .optional(),
 });
